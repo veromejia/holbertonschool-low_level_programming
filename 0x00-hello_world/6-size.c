79d9fb74d0c52c84cdaf2d00ff19f6fb59f08406
veromejia <veromejia.q@gmail.com>
2019-06-19 20:32:34 -0700

    checking the size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ long int longint;
 long long longlong;
 char charType;
 printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of an int: %ld bytes()\n", sizeof(integerType));
-printf("Size of a long int: %ld bytes\n", sizeof(longint));
-printf("Size of a long long int: %ld bytes\n", sizeof(longlong));
-printf("Size of a float: %ld bytes\n", sizeof(floatType));
+printf("Size of an int: %ld byte()\n", sizeof(integerType));
+printf("Size of a long int: %ld byte\n", sizeof(longint));
+printf("Size of a long long int: %ld byte\n", sizeof(longlong));
+printf("Size of a float: %ld byte\n", sizeof(floatType));
 return (0);
 }
