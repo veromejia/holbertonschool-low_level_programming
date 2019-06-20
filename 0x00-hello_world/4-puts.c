f3754a2c142f11eeed641377c150eb7088b82952
veromejia <veromejia.q@gmail.com>
2019-06-19 17:45:45 -0700

    sending a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#include<stdio.h> 
+/*printing a message*/
+#include<stdio.h>
 int main(void) 
-{ 
-    puts("Programming is like building a multilingual puzzle");
-    return (0); 
+{      
+    puts("\"Programming is like building a multilingual puzzle");
+    return(0); 
 } 
