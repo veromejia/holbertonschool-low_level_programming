28796e2647b483c7f80320b0501ec56c25a1121c
veromejia <veromejia.q@gmail.com>
2019-06-20 17:31:41 -0700

    5-printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int count;
+for (count = 0; count <= 9; count++)
+printf("%d", count);
+printf("\n");
+return (0);
+}
