97fc9c7093ef65ec7164b1797252a3415bd803ca
veromejia <veromejia.q@gmail.com>
2019-06-25 12:39:26 -0700

    sending files

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,4 +7,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int c);
+int print_last_digit(int d);
+void jack_bauer(void);
+int add(int a, int b);
 #endif /*HOLBERTON_H*/
