e1727dcb27a8b5673de4e13ea36a38bbf346485e
veromejia <veromejia.q@gmail.com>
2019-06-26 21:48:53 -0700

    times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,21 +6,44 @@
 
 void times_table(void)
 {
-  int i;
-  int j;
-  int result;
-  for (i = 0 ; i < 10; i++)
-    {
-      for (j = 0; j < 10; j++)
-	{
-	  if 
-	  result = i * j;
-	  _putchar(result / 10 + '0');
-	  _putchar(result 
-	  _putchar(',');
-	  _putchar(' ');
-	    }
-      _putchar('\n');
-    }
+int i;
+int j;
+int result;
+for (i = 0 ; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+result = i * j;
+if (result <= 9)
+{
+if (j == 0)
+{
+_putchar('0' + result);
+_putchar(',');
+_putchar (' ');
 }
-      
+else
+{
+_putchar(' ');
+_putchar('0' + result);
+if (j != 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+}
+else
+{
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+if (j != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
+}
+}
