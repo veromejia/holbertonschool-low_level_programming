db081a66c2a161a13eb917679204aa053f784342
veromejia <veromejia.q@gmail.com>
2019-06-27 19:31:02 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,9 +8,22 @@
 void print_diagonal(int n)
 {
 int i;
-for (i = 0; i < n; i++)
+int j;
 if (n > 0)
-  
-_putchar(92);
+{
+for (i = 0; i < n; i++)
+{
+if (i > 0)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+}
+_putchar (92);
+_putchar ('\n');
+}
+}
+else
 _putchar('\n');
- }
+}
