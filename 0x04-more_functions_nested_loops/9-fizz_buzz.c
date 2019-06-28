a2e86806562825bd426dcf71e1e26a393cb93474
veromejia <veromejia.q@gmail.com>
2019-06-27 17:38:40 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,24 +9,16 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
-{
-if (i % 5 == 0)
-printf("FizzBuzz ");
-else
-printf("Fizz ");
-}
-else if (i % 5 == 0)
-{
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 printf("FizzBuzz");
-else
-printf("Buzz ");
-}
-else if (i != 100)
-printf("%d ", i);
+else if (i %3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
 else
 printf("%d", i);
+if (i < 100)
+printf(" ");
 }
 printf("\n");
 return (0);
