cf5e208125333d1925a9ea4b7be5b6ada3a45ac0
veromejia <veromejia.q@gmail.com>
2019-06-27 22:22:21 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,31 +5,27 @@
  * Return: always 0
  */
 
-void print_triangle(int size){
-  int i;
-  int j;
-
-  if (size == 0)
-    _putchar('\n');
-    
-  for(i = 0; i < size; i++)
-  {
-    int spaces;
-    spaces = size - (i+1);
-    for(j = 0; j <= size; j++)
-      {
-
-      if(spaces>j)
-	{
-        _putchar(' ');
-      }
-      if(j>spaces)
-	{
-        _putchar('#');
-      }
-
-    }
-    _putchar('\n');
-  }
-   
+void print_triangle(int size)
+{
+int i;
+int j;
+if (size <= 0)
+_putchar('\n');
+for (i = 0; i < size; i++)
+{
+int spaces;
+spaces = size - (i + 1);
+for (j = 0; j <= size; j++)
+{
+if (spaces > j)
+{
+_putchar(' ');
+}
+if (j > spaces)
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
 }
