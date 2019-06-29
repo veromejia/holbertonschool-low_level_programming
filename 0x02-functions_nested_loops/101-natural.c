7606949c99bb0085658c21a5582e5ef04b7ca95b
veromejia <veromejia.q@gmail.com>
2019-06-29 16:50:11 -0700

    fixed file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,14 +11,14 @@ int n;
 int n3;
 int result;
 int n5;
-n3 =0;
-n5 =0;
+n3 = 0;
+n5 = 0;
 for (n = 0; n < 1024; n++)
 {
 if (n % 3 == 0)
-n3 = n3+ n;
+n3 = n3 + n;
 else
-if (n%5 ==0)
+if (n % 5 == 0)
 n5 = n5 + n;
 result = n3 + n5;
 }
