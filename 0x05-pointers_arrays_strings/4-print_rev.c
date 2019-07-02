dd86fa45b1b7dd4cccb1153b6ed1c08021fcfd0e
veromejia <veromejia.q@gmail.com>
2019-07-02 09:30:50 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_rev -reverse a string
  * @s: string
@@ -12,13 +11,9 @@ i = 0;
 int j;
 int count;
 while (s[i] != 0)
-{
 i++;
-}
 count = i - 1;
 for (j = count; j >= 0; j--)
-{
 _putchar (s[j]);
-}
 _putchar ('\n');
 }
