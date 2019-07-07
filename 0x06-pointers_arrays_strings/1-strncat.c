0980f5f9a184e367ba00eec4b0b22bf98ba2e666
veromejia <veromejia.q@gmail.com>
2019-07-06 18:23:43 -0700

    another fix to 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
+ *_strlen -return the length of a string
+ *@param: string to detereminate the length
+ *Return: a int.
+ */
+int _strlen(char *param)
+{
+int i;
+for (i = 0; *param != '\0'; i++, param++)
+{
+}
+return (i);
+}
+/**
  * _strncat -concatenate 2 string with n limit bytes
  * @dest: destination string to concatenate
  * @src: source string
  * @n: number of bytes to be concatenates
  * Return: a string.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 int i;
 int len;
-for(len=0;dest[i]!='\0'; len++);
-for (i = 0 ;i < n & src[i] != '\0' ; i++){
+len = _strlen(dest);
+for (i = 0; (i < n) & (src[i] != '\0'); i++)
+{
 dest[len + i] = src[i];
 }
 dest[len + i] = '\0';
-return dest;
+return (dest);
 }
