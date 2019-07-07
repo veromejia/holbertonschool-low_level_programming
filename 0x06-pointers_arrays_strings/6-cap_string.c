f13d4a4639ddaed18fc62ac17e7a291f943eaa91
veromejia <veromejia.q@gmail.com>
2019-07-07 06:58:19 -0700

    sending files

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
-char *cap_string(char *a){
-  int i;
-  for(i=0;a[i]!='\0';i++){
-
-    if(a[i]==' ' || a[i]=='\t' || a[i]=='\n' || a[i]==',' || a[i]==';' || a[i]=='.' || a[i]=='!' || a[i]=='?' || a[i]=='"' || a[i]=='(' || a[i]==')' || a[i]=='{' || a[i]=='}'){
-      if(a[i+1]>='a' && a[i+1]<='z'){
-        a[i+1]= a[i+1]-32;
-      }
-    }
-  }
-  return a;
+/**
+ * cap_string -capitalizes all words of a string..
+ * @a: array of integers
+  * Return: a new string.
+ */
+char *cap_string(char *a)
+{
+int i;
+for (i = 0; a[i] != '\0'; i++)
+{
+if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n' || a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i] == '!' || a[i] == '?' || a[i] == '"' || a[i] == ' (' || a[i] == ')' || a[i] == '{' || a[i] == '}')
+{
+if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+{
+a[i + 1] = a[i + 1] - 32;
+}
+}
+}
+return (a);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,16 +1,24 @@
 #include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @a: string
+ * Return: string into 1337.
+ */
 
-char *leet(char *a){
-  int i;
-  int j;
-  char c[]={'A','4','a','4','E','3','e','3','o','0','O','0','t','7','T','7','L','1','l','1'};
-  for(i=0;a[i]!='\0';i++){
-    for(j=0;j<20;j=j+2){
-      if(a[i]==c[j]){
-            a[i]=c[j+1];
-      }
-    }
-
-  }
-  return a;
+char *leet(char *a)
+{
+int i;
+int j;
+char c[] = {'A', '4', 'a', '4', 'E', '3', 'e', '3', 'o', '0', 'O', '0', 't', '7', 'T', '7', 'L', '1', 'l', '1'};
+for (i = 0; a[i] != '\0'; i++)
+{
+for (j = 0; j < 20; j = j + 2)
+{
+if (a[i] == c[j])
+{
+a[i] = c[j + 1];
+}
+}
+}
+return (a);
 }
