39430d5b7a34719c3b1bf58f48b76524bef9e265
veromejia <veromejia.q@gmail.com>
2019-07-08 06:31:22 -0700

    adding a cpy pointer to return

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,34 +1,25 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
- * rot13 - encode a string using root13 algorithm
- * @a: pointer to a char
- * Return: encoded root13 string.
+ * rot13 - reverse_array -reverses the content of an array of integers.
+ * @a: array of integers
+ * Return: encode string.
  */
-
 char *rot13(char *a)
 {
-char aux[] = {'A', 'N', 'B', 'O', 'C', 'P', 'D', 'Q',
-'E', 'R', 'F', 'S', 'G', 'T', 'H', 'U', 'I', 'V', 'J',
-'W', 'K', 'X', 'L', 'Y', 'M', 'Z', 'N', 'A', 'O', 'B',
-'P', 'C', 'Q', 'D', 'R', 'E', 'S', 'F', 'T', 'G', 'U',
-'H', 'V', 'I', 'W', 'J', 'X', 'K', 'Y', 'L', 'Z', 'M',
-'a', 'n', 'b', 'o', 'c', 'p', 'd', 'q', 'e', 'r', 'f',
-'s', 'g', 't', 'h', 'u', 'i', 'v', 'j', 'w', 'k', 'x',
-'l', 'y', 'm', 'z', 'n', 'a', 'o', 'b', 'p', 'c', 'q',
-'d', 'r', 'e', 's', 'f', 't', 'g', 'u', 'h', 'v', 'i',
-'w', 'j', 'x', 'k', 'y', 'l', 'z', 'm'};
-int i;
-int j;
-for (i = 0; a[i] != '\0'; i++)
+char *cpy = a;
+for (; *a != '\0'; a++)
 {
-for (j = 0; j < 64; j = j + 2)
+if ((*a >= 'a' && *a <= 'm') || (*a >= 'A' && *a <= 'M'))
 {
-if (a[i] == aux[j])
+*a = *a + 13;
+}
+else
 {
-a[i] = aux[j + 1];
+while ((*a >= 'n' && *a <= 'z') || (*a >= 'N' && *a <= 'Z'))
+{
+*a = *a - 13;
 }
 }
 }
-return (a);
+return (cpy);
 }
