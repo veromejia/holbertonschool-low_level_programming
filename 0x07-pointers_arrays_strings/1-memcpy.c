22ae4c49a8008009ce97b7d72fa72d39f601e610
veromejia <veromejia.q@gmail.com>
2019-07-09 16:38:51 -0700

    sending files

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,18 +3,18 @@
  *  _memcpy - copy src in dest
  * @dest: pointer of destiny
  * @src: source pointer
- * @n: limit  
+ * @n: limit
  * Return: pointer to char
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 char *p;
-p=dest;
+p = dest;
 unsigned int i;
 for (i = 0; i < n; i++, src++, dest++)
 {
-*dest=*src;
-}  
-return (p);   
+*dest = *src;
+}
+return (p);
 }
