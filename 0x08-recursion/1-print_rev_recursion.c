dc34674e9bd52ab20099dfc4e65f0a8edc2208a0
veromejia <veromejia.q@gmail.com>
2019-07-10 14:30:14 -0700

    print in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+int i = 0;
+if (s[i] != '\0')
+{
+_print_rev_recursion(s + 1);
+putchar(s[i]);
+i++;
+}
+}
