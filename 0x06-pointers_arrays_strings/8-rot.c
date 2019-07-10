3a9c742648a1ed036e526140fedac30693bf2d5d
veromejia <veromejia.q@gmail.com>
2019-07-09 22:57:22 -0700

    fixing 8 file

---
+++ 0x06-pointers_arrays_strings/8-rot.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * root13 - reverse_array -reverses the content of an array of integers.
+ * @a: array of integers
+ * Return: encode string.
+ */
+char *rot13(char *a)
+{
+char *cpy = a;
+for(; *a != '\0'; a++){
+if ((*a >= 'a' && *a <= 'm') || (*a >= 'A' && *a <= 'M'))
+{
+*a = *a + 13;
+}
+else
+{
+while ((*a >= 'n' && *a <= 'z') || (*a >= 'N' && *a <= 'Z'))
+{
+*a = *a - 13;
+}
+}
+}
+return (cpy);
+}
