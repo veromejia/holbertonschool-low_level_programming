84026416a561c2802146fab5a15cf599da256484
veromejia <veromejia.q@gmail.com>
2019-07-13 16:57:21 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,15 @@
 int main(int argc, char *argv[])
 {
 int mul = 1;
-if (argc > 1)
+if (argc == 3)
 {
 mul = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", mul);
 }
 else
+{
 printf("Error\n");
+return (1);
+}
 return (0);
 }
