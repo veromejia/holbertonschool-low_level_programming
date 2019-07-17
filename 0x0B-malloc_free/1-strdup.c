95c417ca1ecda9644668b214dd5e792b5c177413
veromejia <veromejia.q@gmail.com>
2019-07-16 17:25:54 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strdup - copy of the string given as a parameter.
+ *@str: string
+ *Return: a pointer to a new string which is a duplicate of the string str.
+ */
+char *_strdup(char *str)
+{
+char *ptr;
+int size;
+int j;
+size = 0;
+if (str == NULL)
+return (NULL);
+while (str[size] != '\0')
+size++;
+if (size < 1)
+return (NULL);
+ptr = malloc(size *sizeof(char));
+if (ptr == NULL)
+{
+free(ptr);
+return (NULL);
+}
+for (j = 0; j < size; j++)
+ptr[j] = str[j];
+ptr[j] = '\0';
+return (ptr);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,4 +2,5 @@
 #define _HOLBERTON_H_
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
