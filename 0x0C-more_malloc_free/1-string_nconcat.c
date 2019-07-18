863cdc98450d06df7fa1bf6f9f41e4e3d72f4797
veromejia <veromejia.q@gmail.com>
2019-07-18 15:58:15 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,8 +13,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *ptr;
 unsigned int i, j, k;
+if (s1 == NULL)
+s1 = "";
 if (s2 == NULL)
-s2 = " ";
+s2 = "";
 for (i = 0; s1[i] != '\0'; i++)
 {
 }
