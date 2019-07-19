21beca1bbffd8d69e5226a174efc2716cd3ac1a8
veromejia <veromejia.q@gmail.com>
2019-07-18 18:02:01 -0700

    new calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc -allocate memory for nmemb for an array
+ * @nmemb: number os memor of bytes
+ * @size: size of the array
+ * Return: array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+return (NULL);
+char *ptr_char = (char *)ptr;
+for (i = 0; i < (nmemb * size); i++)
+ptr_char[i] = 0;
+return (ptr_char);
+}
