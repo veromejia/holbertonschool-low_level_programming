c4fec29ca4714ff35a996b350e6b1a26d15866bb
veromejia <veromejia.q@gmail.com>
2019-07-22 21:33:20 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,7 +7,7 @@
  * @age: age of new dog
  * @owner: owner of the new dog
  * Return: a pointer of type struct dog (name, age,owner)
- */ 
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -15,9 +15,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 struct dog *new_dog;
 new_dog = malloc(sizeof(struct dog));
 if (new_dog == NULL)
-return(NULL);
-new_dog ->name = name;
-new_dog ->age = age;
-new_dog ->owner = owner;
+return (NULL);
+new_dog->name = name;
+new_dog->age = age;
+new_dog->owner = owner;
 return (new_dog);
 }
