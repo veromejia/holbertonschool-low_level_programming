416c4792123ce9c409aabecdee38e5bf9e843990
veromejia <veromejia.q@gmail.com>
2019-07-24 22:19:24 -0700

    fix calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 int result;
 if (argc != 4)
 {
-printf("error\n");
+printf("Error\n");
 exit(98);
 }
-if (argv[2][1] != '\0')
+if (get_op_func(argv[2]) == NULL)
 {
-printf("error\n");
-exit(100);
+printf("Error\n");
+exit(99);
 }
 result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 printf("%d\n", result);
