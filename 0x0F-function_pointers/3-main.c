ad662c296202a795d3eb54e4caf843384a6e5c7b
veromejia <veromejia.q@gmail.com>
2019-07-24 20:48:58 -0700

    calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *main - principal function
+ * @argc: numbers of parameters
+ * @argv: arguments
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+int result;
+if (argc != 4)
+{
+printf("error\n");
+exit(98);
+}
+if (argv[2][1] != '\0')
+{
+printf("error\n");
+exit(100);
+}
+result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+printf("%d\n", result);
+return (0);
+}
