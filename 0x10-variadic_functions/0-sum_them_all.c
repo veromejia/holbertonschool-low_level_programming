561057cbcd52ed975fb4dda2e5524c6330d50ee6
veromejia <veromejia.q@gmail.com>
2019-07-27 16:18:57 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,10 +16,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int sum = 0;
 	unsigned int i;
 
+	va_start(numbers_to_add, n);
+
+
 	if (n == 0)
 		return (0);
 
-	va_start(numbers_to_add, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(numbers_to_add, int);
 
