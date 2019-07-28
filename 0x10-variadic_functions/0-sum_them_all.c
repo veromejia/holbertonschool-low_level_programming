ca1a08aaeeafd4820fa2a7597bcd0e48a66c4771
veromejia <veromejia.q@gmail.com>
2019-07-27 18:17:29 -0700

    sum fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-va_list numbers;
-unsigned int i, sum;
-sum = 0;
-va_start(numbers, n);
-if (n == 0)
-return (0);
-for (i = 0; i < n; i++)
-sum += va_arg(numbers, int);
-va_end(numbers);
-return (sum);
+	if (n > 0)
+	{
+		va_list numbers;
+		unsigned int i, sum;
+
+		sum = 0;
+		va_start(numbers, n);
+		for (i = 0; i < n; i++)
+			sum += va_arg(numbers, int);
+		va_end(numbers);
+		return (sum);
+	}
+	return (0);
 }
