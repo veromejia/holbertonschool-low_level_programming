5abc16d1568d0dcbb6cbb121ceb68a665a4e1e21
veromejia <veromejia.q@gmail.com>
2019-07-27 17:35:04 -0700

    new file

--- 0x10-variadic_functions/0-main.c
+++ 0x10-variadic_functions/0-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
     int sum;
 
-    sum = sum_them_all(0, 98, 1024);
+    sum = sum_them_all(2, 98, 1024);
     printf("%d\n", sum);
-    sum = sum_them_all(4, 'a', 1024, 402, -1024);
+    sum = sum_them_all(4, 98, 1024, 402, -1024);
     printf("%d\n", sum);
     return (0);
 }
---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - add the value of the list of parameters
+ * @n: numbers to add
+ * Return: addition of the list of numbers
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list numbers;
+	unsigned int i, sum;
+
+	va_start(numbers, n);
+	if (n == 0)
+		return (0);
+	for (i = sum = 0; i < n; i++)
+	{
+		sum += va_arg(numbers, int);
+	}
+	va_end(numbers);
+	return (sum);
+}
