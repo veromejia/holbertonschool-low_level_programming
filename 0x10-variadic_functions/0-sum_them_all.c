3b5be467179bbea3bc5f283440357d5ae5a09913
veromejia <veromejia.q@gmail.com>
2019-07-27 17:42:33 -0700

    fix add

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,6 +5,7 @@
 /**
  * sum_them_all - add the value of the list of parameters
  * @n: numbers to add
+ * @...: arguments
  * Return: addition of the list of numbers
  */
 
@@ -13,13 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i, sum;
 
+	sum = 0;
 	va_start(numbers, n);
+
 	if (n == 0)
 		return (0);
-	for (i = sum = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++)
 		sum += va_arg(numbers, int);
-	}
 	va_end(numbers);
 	return (sum);
 }
