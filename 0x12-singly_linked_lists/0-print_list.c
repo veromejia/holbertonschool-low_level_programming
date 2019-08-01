944127c93958ccaa9e2c513be0963f2eeb7a74c3
veromejia <veromejia.q@gmail.com>
2019-08-01 15:30:10 -0700

    printing a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ * @h: list to be print
+ * Return: number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 1; h->next != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[%u] %s\n", h->len, "(nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	printf("[%u] %s\n", h->len, h->str);
+	return (i);
+}
