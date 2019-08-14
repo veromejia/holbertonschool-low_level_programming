eb40fa71cc0b9767da70e74d696cb539de4066ad
veromejia <veromejia.q@gmail.com>
2019-08-13 19:09:15 -0700

    fix file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,6 +10,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, i;
 
+	if (filename == NULL)
+		return (-1);
+
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
