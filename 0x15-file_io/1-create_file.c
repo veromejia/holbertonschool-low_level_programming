6878741438cd755dda16b2929f3dd8d79c2e0015
veromejia <veromejia.q@gmail.com>
2019-08-13 19:01:09 -0700

    creating file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * create_file - function that create a file
+ * @filename: file to be created
+ * @text_content: content of the file
+ * Return: 1 if success -1 if fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, i;
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	write(file, text_content, i);
+	close(file);
+	return (1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,10 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdio.h>
+
+#include "holberton.h"
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /*HOLBERTON_H*/
