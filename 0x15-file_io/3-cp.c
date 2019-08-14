dc4ea89d6da1c10102c8321f166df637e045e7bd
veromejia <veromejia.q@gmail.com>
2019-08-13 23:22:09 -0700

    fixing file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,13 +17,9 @@ int main(int ac, char **av)
 	if (file_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_EXCL, 0664);
+	file_to = open(av[2], O_WRONLY | O_CREAT, 0664);
 	if (file_to == -1)
-		open(av[2], O_WRONLY | O_TRUNC);
-	if (file_to == -1)
-		dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", av[2]);
-			exit(99);
+		open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (file_from != -1)
 	{
