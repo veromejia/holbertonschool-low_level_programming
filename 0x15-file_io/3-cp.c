7fb377dc4e1056dce94c41e1dfb195796f9f13c4
veromejia <veromejia.q@gmail.com>
2019-08-13 22:57:03 -0700

    coppy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * @ac: number of arguments
+ * @av: arguments
+ * Return: Always 0 (Success)
+ */
+int main(int ac, char **av)
+{
+	int file_from, file_to, rd, wr;
+	char buffer[1024];
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+	file_to = open(av[2], O_WRONLY | O_CREAT, 0664);
+	if (file_to == -1)
+		open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (file_from != -1)
+	{
+		rd = 1;
+		while (rd)
+		{
+			rd = read(file_from, buffer, 1024);
+			if (rd == -1)
+				dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n",
+				 av[1]), exit(98);
+			wr = write(file_to, buffer, rd);
+			if (wr != rd || wr == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		}
+	}
+
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+
+	return (0);
+}
