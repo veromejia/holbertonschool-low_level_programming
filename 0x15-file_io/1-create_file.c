d894a723daf6e486036026850c39597da9b48b9c
veromejia <veromejia.q@gmail.com>
2019-08-13 19:52:32 -0700

    adding condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,9 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
+
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
