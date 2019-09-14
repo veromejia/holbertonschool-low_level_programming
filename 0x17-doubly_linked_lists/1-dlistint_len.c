cc75db44133ac75c1e7e53721c8aa7c647bbb759
veromejia <veromejia.q@gmail.com>
2019-09-14 14:37:44 -0700

    fixed betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * dlistint_len - function the show the len of the list
+ * @h: list
+ * Return: the lenght of the list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i;
