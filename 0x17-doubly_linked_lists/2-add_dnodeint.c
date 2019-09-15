0dbcc7fb7dfbf087cceea298f7c6415063612a14
veromejia <veromejia.q@gmail.com>
2019-09-14 18:02:59 -0700

    add new case

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,14 +12,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node = *head;
 
+	if (head == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	if (*head == NULL)
-	{
 		new_node->next = NULL;
-	}
 	else
 		new_node->n = n;
 	new_node->prev = NULL;
