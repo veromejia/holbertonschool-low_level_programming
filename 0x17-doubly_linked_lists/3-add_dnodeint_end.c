4e93b323f134603151e1db87794efdaa072672ba
veromejia <veromejia.q@gmail.com>
2019-09-14 20:07:25 -0700

    fixed file

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,18 +18,18 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
-		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
 	while (tmp->next)
 		tmp = tmp->next;
 	tmp->next = new_node;
-	tmp->prev = *head;
-	return (tmp);
+	new_node->prev = tmp;
+	return (new_node);
 }
