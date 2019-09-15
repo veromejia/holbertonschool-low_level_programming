4e9cc36154af96b73dcaffcf0d0a35e9bfc9713b
veromejia <veromejia.q@gmail.com>
2019-09-14 20:29:51 -0700

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint_t - free memory
+ * @head: the beginning of hte list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+
+	if (head == NULL)
+		return;
+	while (head->next)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
+		free(head);
+}
