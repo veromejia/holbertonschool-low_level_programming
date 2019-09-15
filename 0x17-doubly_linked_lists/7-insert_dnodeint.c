16a770f476f7dd6e2549bc0b03496fe2eef84be5
veromejia <veromejia.q@gmail.com>
2019-09-15 13:36:54 -0700

    adding idx !=0 condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
