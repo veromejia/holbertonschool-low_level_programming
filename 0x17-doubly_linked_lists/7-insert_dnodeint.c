4eaec4ac28174cfce99f38f4e09e6cd55fa4f69e
veromejia <veromejia.q@gmail.com>
2019-09-15 12:13:57 -0700

    add new case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	for (i = 0; i < idx - 1; i++)
 	{
 		temp = temp->next;
