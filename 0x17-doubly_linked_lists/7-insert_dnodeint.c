5807f30c599a820616b84879da7c365dbc8122ab
veromejia <veromejia.q@gmail.com>
2019-09-23 20:14:29 -0700

    fixed betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 
-	if (*h == NULL && idx !=0)
+	if (*h == NULL && idx != 0)
 		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
