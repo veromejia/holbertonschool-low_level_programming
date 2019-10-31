7aa31b90baa31bea5394f96543c04cb488e5707d
veromejia <veromejia.q@gmail.com>
2019-10-30 21:08:08 -0700

    fix betty

--- 0x1B-sorting_algorithms/104-heap_sort.c
+++ 0x1B-sorting_algorithms/104-heap_sort.c
@@ -9,23 +9,23 @@
  */
 void heapify(int *array, int n, int i, size_t size)
 {
-        int largest = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        int temp;
+	int largest = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+	int temp;
 
-        if (l < n && array[l] > array[largest])
-                largest = l;
-        if (r < n && array[r] > array[largest])
-                largest = r;
-        if (largest != i)
-        {
-                temp = array[i];
-                array[i] = array[largest];
-                array[largest] = temp;
-                print_array(array, size);
-                heapify(array, n, largest, size);
-        }
+	if (l < n && array[l] > array[largest])
+		largest = l;
+	if (r < n && array[r] > array[largest])
+		largest = r;
+	if (largest != i)
+	{
+		temp = array[i];
+		array[i] = array[largest];
+		array[largest] = temp;
+		print_array(array, size);
+		heapify(array, n, largest, size);
+	}
 }
 
 /**
@@ -38,18 +38,18 @@ void heapify(int *array, int n, int i, size_t size)
 
 void heap_sort(int *array, size_t size)
 {
-        int i;
-        int temp;
+	int i;
+	int temp;
 
-        for (i = size / 2; i >= 0; i--)
-                heapify(array, size, i, size);
-        for (i = size - 1; i >= 0; i--)
-        {
-                temp = array[0];
-                array[0] = array[i];
-                array[i] = temp;
-                if (i != 0)
-                        print_array(array, size);
-                heapify(array, i, 0, size);
-        }
+	for (i = size / 2; i >= 0; i--)
+		heapify(array, size, i, size);
+	for (i = size - 1; i >= 0; i--)
+	{
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+		if (i != 0)
+			print_array(array, size);
+		heapify(array, i, 0, size);
+	}
 }
