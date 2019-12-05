baa6c4f9cd16af0774a12fe04c28bd5c869b36e5
veromejia <veromejia.q@gmail.com>
2019-12-04 23:25:09 -0800

    fixing variable

--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -12,6 +12,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL || tree->right != NULL)
-		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->left) + 1);
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 	return (0);
 }
